71e966148081280d31534852fa7078973bb947f7
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-10 22:24:45 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -45,13 +45,13 @@ def naming_system
               label: "Genus",
               sub_category: {
                 label: "Species",
-                sub_category: nil }
-                }
-                }
-                }
-                }
-                }
-                }
+                sub_category: {} }
+          }
+        }
+      }
+    }
+ }
+}
 
 end
  # Remember:
